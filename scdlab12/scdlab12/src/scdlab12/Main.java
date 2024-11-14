232dd77c7c70cb2cfd203d488c3cba2eb37cf801
Eman Nabi <f223626@cfd.nu.edu.pk>
2024-11-14 11:22:18 +0500

    Revert "Introduced a bug"
    This reverts commit 92e805a6eaf1669344d2e49ae59559d937673312.

--- scdlab12/scdlab12/src/scdlab12/Main.java
+++ scdlab12/scdlab12/src/scdlab12/Main.java
@@ -1,11 +1,12 @@
 package scdlab12;
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the application!");
-        // greetUser(); // Removed this line to introduce a bug
-    }
 
-    public static void greetUser() {
-        System.out.println("Hello, User!");
-    }
+public class Main {
+public static void main(String[] args) {
+System.out.println("Welcome to the application!");
+greetUser();
+}
+
+public static void greetUser() {
+System.out.println("Hello, User!");
+}
 }
