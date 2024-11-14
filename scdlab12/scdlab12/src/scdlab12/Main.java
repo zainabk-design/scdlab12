92e805a6eaf1669344d2e49ae59559d937673312
Eman Nabi <f223626@cfd.nu.edu.pk>
2024-11-14 11:19:50 +0500

    Introduced a bug

--- scdlab12/scdlab12/src/scdlab12/Main.java
+++ scdlab12/scdlab12/src/scdlab12/Main.java
@@ -1,12 +1,11 @@
 package scdlab12;
-
 public class Main {
-public static void main(String[] args) {
-System.out.println("Welcome to the application!");
-greetUser();
-}
+    public static void main(String[] args) {
+        System.out.println("Welcome to the application!");
+        // greetUser(); // Removed this line to introduce a bug
+    }
 
-public static void greetUser() {
-System.out.println("Hello, User!");
-}
+    public static void greetUser() {
+        System.out.println("Hello, User!");
+    }
 }
