0a538157bbfb2221b049834c3ffcb2f4cbe79ab0
zainab khurram <f223691@cfd.nu.edu.pk>
2024-11-14 11:13:55 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package scdlab12;
+
+public class Main {
+public static void main(String[] args) {
+System.out.println("Welcome to the application!");
+greetUser();
+}
+
+public static void greetUser() {
+System.out.println("Hello, User!");
+}
+}
